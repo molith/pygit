303f2835ba945ec26ee51e5af136edb3b61bc3ca
molith <temenseperjuangan@gmail.com>
2018-01-07 06:39:20 +0700

    Delete exc3 If Else.py

--- python3/exc3 If Else.py
+++
@@ -1,8 +0,0 @@
-negara = input("Ketik nama negara\n")
-merdeka = int(input("Telah merdeka selama berapa tahun?\n"))
-if negara == "Indonesia" or "indonesia":
-    print("Negaraku {} telah merdeka selama {} tahun".format(negara, merdeka))
-elif negara == "Narnia" or "narnia":
-    print("Negri {} telah merdeka selama {} tahun".format(negara, merdeka))
-else:
-    print("{} telah merdeka selama {} tahun".format(negara, merdeka))
